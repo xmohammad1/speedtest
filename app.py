ef6f8405aba5bed2228eb994d2a4c1354cd6c77f
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-08-25 15:52:18 +0330

    Revert "Measure ping over TCP"

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def ping():
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
-    response.headers['Connection'] = 'close'
     return response
 
 
