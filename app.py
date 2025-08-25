fd695ad1a0de3b9a28563fac5015a78dcb5b6a89
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-08-25 05:18:17 -0700

    Measure ping over TCP

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def ping():
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
+    response.headers['Connection'] = 'close'
     return response
 
 
